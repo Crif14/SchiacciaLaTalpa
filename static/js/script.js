f5decdc0f96e3596a83a9e47576e0feb6963c53c
Crif14 <csqueo22@gmail.com>
2025-01-06 22:35:48 +0100

    comparsa e scomparsa menu

--- static/js/script.js
+++ static/js/script.js
@@ -1,19 +1,2 @@
-let selectedDifficulty = null;
 
-function startGame() {
-    if (!selectedDifficulty) {
-        document.getElementById('difficultyDropdown').classList.remove('is-primary');
-        document.getElementById('difficultyDropdown').classList.add('is-error');
-        alert('Seleziona una difficoltà prima di avviare il gioco!');
-    } else {
-        alert('Gioco avviato con difficoltà: ' + selectedDifficulty);
-    }
-}
 
-function setDifficulty(level) {
-    selectedDifficulty = level;
-    document.getElementById('difficultyDropdown').classList.remove('btn-danger');
-    document.getElementById('difficultyDropdown').classList.add('btn-secondary');
-    document.getElementById('difficultyDropdown').innerText = 'Difficoltà: ' + level;
-    alert('Difficoltà impostata su: ' + level);
-}
